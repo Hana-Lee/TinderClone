c55288c82ab93b6949734435c3bc289fa7a0d749
Hana Lee <i@leehana.co.kr>
2015-05-25 01:25:18 +0900

    Initial Commit

---
+++ TinderClone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinderClone
+//
+//  Created by Hana Lee on 2015. 5. 25..
+//  Copyright (c) 2015년 Hana Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
